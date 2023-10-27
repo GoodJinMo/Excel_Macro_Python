8b8bac061070919bce3060d6bef19c27f839e68c
GoodJinMo <97722297+GoodJinMo@users.noreply.github.com>
2023-10-27 19:11:47 +0900

    Create style.py

---
+++ style.py
@@ -0,0 +1,27 @@
+theme="""
+QPushButton{                        /* Button */
+	color: rgb(58, 134, 255);
+	background-color: white;
+	border: 2px solid rgb(58, 134, 255);
+	border-radius: 20px;
+}
+ QPushButton:hover {
+    background-color: #8DEBFA;
+    border: 2px solid #82D8E6; 
+}
+QPushButton:pressed {
+    background-color: #52A6B4; 
+    border: 2px solid #4C99A5; 
+}
+QWidget {                           /* Background*/
+    background-color: #FFFFFF;
+    font-size: 14px;
+}
+
+QLineEdit{                          /* LineEdit */
+    background-color: lightgray; 
+    border: 2px solid rgb(58, 134, 255); 
+    border-radius: 5px; 
+    padding: 2px;
+    }
+    """
